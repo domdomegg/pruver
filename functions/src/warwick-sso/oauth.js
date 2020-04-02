f5c90be94a652817e03ee7b75fd0669fba08b6be
Adam Jones <jones_adam@rocketmail.com>
2020-04-02 20:22:15 +0100

    Add Warwick's websignon-test to dev (#2)

--- functions/src/warwick-sso/oauth.js
+++ functions/src/warwick-sso/oauth.js
@@ -2,8 +2,8 @@ const oauthConfig = require('../../config/oauth.json');
 
 const Oauth = require('oauth');
 const oauth = new Oauth.OAuth(
-  'https://websignon.warwick.ac.uk/oauth/requestToken',
-  'https://websignon.warwick.ac.uk/oauth/accessToken',
+  oauthConfig.warwickSso.requestTokenUrl,
+  oauthConfig.warwickSso.accessTokenUrl,
   oauthConfig.warwickSso.consumerKey,
   oauthConfig.warwickSso.consumerSecret,
   '1.0',
